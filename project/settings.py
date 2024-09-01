499ae012994aa198faf3016d30fdfcb2289e3ce9
Diego Lizarazo <dalzoj@MacBook-Air-de-Diego.local>
2024-08-31 20:05:48 -0500

    Configuración en Settings
    Configuración de SecretKey en el proyecto

--- project/settings.py
+++ project/settings.py
@@ -11,22 +11,23 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t)&7!eza4kye+%+&4_uy32tp6(e$hkv*_s1hul+3v&y+z#)o!$'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
